c91dd3bb5cbb7f043f7e144d7e5d73bf1cdee53a
marshall7m <44485531+marshall7m@users.noreply.github.com>
2021-05-31 13:50:20 -0700

    use tmp tf module for tg source parsing

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,7 @@
+module "sub_module" {
+  source = ".//sub_module"
+}
+
 output "foo" {
     value = "foo"
 }
--- terraform_tests.sh
+++ terraform_tests.sh
@@ -59,74 +59,70 @@ fi
 modified_dirs=$(echo "${modified_dirs[*]}" | tr ' ' '\n' | sort -u)
 
 test_dirs=()
-# gets directory paths within `tests/` that contain .tf or .hcl files
+# gets directory paths within `tests/` that contain .tf or .hcl files but ignore .terra*/
 for filepath in $(find tests/ -type d \( -name .terragrunt-cache -o -name .terraform \) -prune -false -o -name '*.hcl' -or -name '*.tf'); do
     #trim extra slashes
     filepath=$(echo "${filepath// /__REPLACED__SPACE__}" | tr -s /)
     test_dirs+="$(dirname $filepath) "
 done
 
-# get unique directories
+# get unique test sub directories
 declare -a test_dirs=$(echo $test_dirs | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
 for tf_dir in $test_dirs; do
-    echo
-    echo "before pwd: "
-    pwd
     cd "$tf_dir" > /dev/null
+    echo ""
     binary=$(infer_binary ".") || exit
     logger "Inferred binary: ${binary}" "INFO"
     logger "${binary} directory: ${tf_dir}" "DEBUG"
 
-    #TODO: rm once fixed:
-
+# use terragrunt source path as module_dep_paths
     if [ "${binary}" == "terragrunt" ]; then
-        #TODO: Figure out why this doesn't work `cd $(terragrunt terragrunt-info | jq .WorkingDir)`
-        tg_cache_dir=$(terragrunt terragrunt-info | jq .WorkingDir)
-        #TODO figure out why can't cd into $tg_cache_dir
-        cd $tg_cache_dir
+        # get local terraform module source path from terragrunt file to list
+        tg_dep_path=$(grep -Pzo 'terraform\s+\{(\s+.+)+source\s+=\s+\"\K(\.\.\/+)+.+(?=\"\s+\})' terragrunt.hcl |  sed 's/\/\//\//g')
 
-        # loads/updates terraform module sources to `.terraform/`
-        terraform get -update
-        # parse modules.json to get module source paths
-        module_dep_paths=$(python -c "import json; print(str({module['Dir'] for module in [json.loads(line) for line in open('.terraform/modules/modules.json', 'r').read().split('\n')][0]['Modules']}).replace('{', '').replace('}', '').replace(\"'\", '').replace(',', ''))")
-
-        cd - > /dev/null
-    else
-        terraform get -update
-        # parse modules.json to get module source paths
-        module_dep_paths=$(python -c "import json; print(str({module['Dir'] for module in [json.loads(line) for line in open('.terraform/modules/modules.json', 'r').read().split('\n')][0]['Modules']}).replace('{', '').replace('}', '').replace(\"'\", '').replace(',', ''))")
+        # create tmp tf module file that uses the tg source path
+        cat <<EOF > tmp.tf
+module "test" {
+    source = "${tg_dep_path}"
+}
+EOF
     fi
 
     # loads/updates terraform module sources to `.terraform/`
     terraform get -update
     # parse modules.json to get module source paths
     module_dep_paths=$(python -c "import json; print(str({module['Dir'] for module in [json.loads(line) for line in open('.terraform/modules/modules.json', 'r').read().split('\n')][0]['Modules']}).replace('{', '').replace('}', '').replace(\"'\", '').replace(',', ''))")
-
-
     logger "Module dependency sources:" "DEBUG"
     logger "${module_dep_paths}" "DEBUG"
+
+    if [ "${binary}" == "terragrunt" ]; then
+        #clean up .tf files before running terragrunt cmd
+        rm tmp.tf
+    fi
 
     rel_paths=()
     for dir in ${modified_dirs[@]}; do
         logger "modified directory path: ${dir}" "DEBUG"
-        # gets relative path of the terraform directory to the modified paths
+        # gets relative path of the test directory to the modified paths
         rel_paths+=$(realpath --relative-to=${PWD} ${dir})
     done
 
     logger "relative path list: ${rel_paths[@]}" "DEBUG"
 
     for i in "${!rel_paths[@]}"; do
+        logger "Searching for: ${rel_paths[$i]} within Terraform module dependency paths: ${module_dep_paths[@]}"
         if [[ " ${module_dep_paths[@]} " =~ " ${rel_paths[$i]} " ]]; then
-            echo "A module dependency within directory: ${tf_dir} has changed"
+            echo "A module dependency within directory: '${tf_dir}' has changed"
 
             cmd="${binary} test"
-            echo "Running command: '${cmd}' within the directory: ${tf_dir}"
+            echo "Running command: '${cmd}' within the directory: '${tf_dir}'"
             $cmd
             break
         fi
 
         if [[ "${i+1}" ==  ${#rel_paths[@]} ]]; then
+            echo "No module dependencies were changed within directory '${tf_dir}'"
             echo "Skipping command: '${cmd}' within the directory: ${tf_dir}"
         fi
     done
